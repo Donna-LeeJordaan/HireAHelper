432773379d1eee82a992fa825ff72befa01d3175
moega <moegamattauriqosman@gmail.com>
2025-05-18 15:25:17 +0200

    updated Service Provider Domain Service Provider Factory Some validation in helper

--- HireAHelper/src/main/java/za/co/hireahelper/util/Helper.java
+++ HireAHelper/src/main/java/za/co/hireahelper/util/Helper.java
@@ -2,68 +2,42 @@ package za.co.hireahelper.util;
 
 public class Helper {
 
-
-
-
-
-
-    public static boolean isNullOrEmpty(String str) {
-        return str == null || str.isEmpty();
+    public static boolean isNullOrEmpty(String str) {return str == null || str.isEmpty();
     }
 
-    // Todo: isValidPostalCode method - 4 digits with range 1000 to 9999
-    public static boolean isValidPostalCode(short postalCode) {
-        if (postalCode < 1000 || postalCode > 9999) {
+    public static boolean isValidEmail(String email) {
+        if (isNullOrEmpty(email)) {
             return false;
         }
-        return true;
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        return email.matches(emailRegex);
     }
 
-    // Todo: isValidStreetNumber method - 1 to 5 digits with range 1 to 99999
-    public static boolean isValidStreetNumber(short streetNumber) {
-        if (streetNumber < 1 || streetNumber > 99999) {
-            return false;
-        }
-        return true;
+    public static boolean isValidRate(double rate) {
+        return rate > 0;
     }
-
-
-    // Todo: isValidErfNumber method - 1 to 5 digits with range 1 to 99999
-    public static boolean isValidErfNumber(int erfNumber) {
-        if (erfNumber < 1 || erfNumber > 99999) {
-            return false;
-        }
-        return true;
-    }
-
-    // Todo: isValidUnitNumber method - 1 to 5 digits with range 1 to 99999
-    public static boolean isValidUnitNumber(short unitNumber) {
-        if (unitNumber < 1 || unitNumber > 99999) {
-            return false;
-        }
-        return true;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
