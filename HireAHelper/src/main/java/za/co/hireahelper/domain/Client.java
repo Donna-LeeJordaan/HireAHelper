de77b557377663ba5afc820af29b6fd300af8ee2
Saadiqah Hendricks <saadiqahhendricks2909@gmail.com>
2025-05-18 14:55:11 +0200

    Saadiqah Domain Entities Complete.

--- HireAHelper/src/main/java/za/co/hireahelper/domain/Client.java
+++ HireAHelper/src/main/java/za/co/hireahelper/domain/Client.java
@@ -1,4 +1,75 @@
+/* User.java
+
+   Author: S Hendricks (221095136)
+
+   Date: 18 May 2025 */
+
 package za.co.hireahelper.domain;
 
-public class Client {
+import java.util.List;
+
+public class Client extends User {
+
+    private List<Booking> bookings;
+    private List<Message> messages;
+
+    private Client() {
+        super();
+    }
+
+    private Client(Builder builder) {
+        super(builder);
+        this.bookings = builder.bookings;
+        this.messages = builder.messages;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                super.toString() +
+                ", bookings=" + bookings +
+                ", messages=" + messages +
+                '}';
+    }
+
+    public static class Builder extends User.Builder<Builder> {
+        private List<Booking> bookings;
+        private List<Message> messages;
+
+        public Builder setBookings(List<Booking> bookings) {
+            this.bookings = bookings;
+            return this;
+        }
+
+        public Builder setMessages(List<Message> messages) {
+            this.messages = messages;
+            return this;
+        }
+
+        public Builder copy(Client client) {
+            super.copy(client);
+            this.bookings = client.bookings;
+            this.messages = client.messages;
+            return this;
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        @Override
+        public Client build() {
+            return new Client(this);
+        }
+    }
 }
+
--- HireAHelper/src/main/java/za/co/hireahelper/domain/User.java
+++ HireAHelper/src/main/java/za/co/hireahelper/domain/User.java
@@ -1,4 +1,105 @@
+/* User.java
+
+   Author: S Hendricks (221095136)
+
+   Date: 18 May 2025 */
+
 package za.co.hireahelper.domain;
 
-public class User {
+public abstract class User {
+
+    private String userId;
+    private String name;
+    private String email;
+    private String password;
+    private String mobileNumber;
+
+    protected User() {
+    }
+
+    protected User(Builder<?> builder) {     //Builder<?> necessary to make the constructor flexible enough to work with any subclass.
+        this.userId = builder.userId;
+        this.name = builder.name;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.mobileNumber = builder.mobileNumber;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                '}';
+    }
+
+    public static abstract class Builder<T extends Builder<T>> {  //Generic <T> pattern in the abstract User is better for inheritance and fluent chaining in subclasses.
+        private String userId;
+        private String name;
+        private String email;
+        private String password;
+        private String mobileNumber;
+
+        public T setUserId(String userId) {
+            this.userId = userId;
+            return self();
+        }
+
+        public T setName(String name) {
+            this.name = name;
+            return self();
+        }
+
+        public T setEmail(String email) {
+            this.email = email;
+            return self();
+        }
+
+        public T setPassword(String password) {
+            this.password = password;
+            return self();
+        }
+
+        public T setMobileNumber(String mobileNumber) {
+            this.mobileNumber = mobileNumber;
+            return self();
+        }
+
+        public T copy(User user) {
+            this.userId = user.userId;
+            this.name = user.name;
+            this.email = user.email;
+            this.password = user.password;
+            this.mobileNumber = user.mobileNumber;
+            return self();
+        }
+
+        protected abstract T self();
+
+        public abstract User build();
+    }
 }
+
