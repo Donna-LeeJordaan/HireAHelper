82d15361341bbc8128257a6a4042256bec382e99
gabrielkiewietz <kiewietzg16@gmail.com>
2025-05-18 22:04:01 +0200

    newpush

--- HireAHelper/src/main/java/za/co/hireahelper/factory/ServiceTypeFactory.java
+++ HireAHelper/src/main/java/za/co/hireahelper/factory/ServiceTypeFactory.java
@@ -1,4 +1,18 @@
 package za.co.hireahelper.factory;
 
+import za.co.hireahelper.domain.ServiceType;
+
 public class ServiceTypeFactory {
+
+    public static ServiceType createServiceType(String typeId, String typeName) {
+        if (typeId == null || typeId.isEmpty()
+                || typeName == null || typeName.isEmpty()) {
+            return null;
+        }
+
+        return new ServiceType.Builder()
+                .setTypeId(typeId)
+                .setTypeName(typeName)
+                .build();
+    }
 }
