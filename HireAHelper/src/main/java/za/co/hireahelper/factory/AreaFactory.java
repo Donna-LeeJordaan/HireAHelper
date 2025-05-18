84726a74eeca341218434d89db17add2a7de804a
Ameeruddin <ameerarai7@gmail.com>
2025-05-18 21:26:18 +0200

    updated code

--- HireAHelper/src/main/java/za/co/hireahelper/factory/AreaFactory.java
+++ HireAHelper/src/main/java/za/co/hireahelper/factory/AreaFactory.java
@@ -1,4 +1,20 @@
 package za.co.hireahelper.factory;
 
+//ameeruddin arai 230190839
+
+import za.co.hireahelper.domain.Area;
+import za.co.hireahelper.util.Helper;
+
 public class AreaFactory {
+
+    public static Area createArea(String areaId, String name) {
+        if (Helper.isNullOrEmpty(areaId) || Helper.isNullOrEmpty(name)) {
+            return null;
+        }
+
+        return new Area.Builder()
+                .setAreaId(areaId)
+                .setName(name)
+                .build();
+    }
 }
