a8e2dd8b43785ce9b1b8718ec031e64cbbb41cee
gabrielkiewietz <kiewietzg16@gmail.com>
2025-05-19 10:54:26 +0200

    newpush

--- HireAHelper/src/test/java/za/co/hireahelper/factory/MessageFactoryTest.java
+++ HireAHelper/src/test/java/za/co/hireahelper/factory/MessageFactoryTest.java
@@ -1,6 +1,30 @@
 package za.co.hireahelper.factory;
 
+import org.junit.jupiter.api.Test;
+import za.co.hireahelper.domain.Message;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageFactoryTest {
+    @Test
+    void testCreateValidMessage() {
+        Message message = MessageFactory.createMessage("msg123", LocalDateTime.now(), "Hello!");
+        assertNotNull(message);
+        assertEquals("msg123", message.getMessageId());
+        assertEquals("Hello!", message.getContent());
+    }
 
+    @Test
+    void testCreateMessageWithNullValues() {
+        Message message = MessageFactory.createMessage(null, LocalDateTime.now(), "Hello!");
+        assertNull(message);
+    }
+
+    @Test
+    void testCreateMessageWithEmptyContent() {
+        Message message = MessageFactory.createMessage("msg123", LocalDateTime.now(), "");
+        assertNull(message);
+    }
 }
--- HireAHelper/src/test/java/za/co/hireahelper/factory/ServiceTypeTest.java
+++ HireAHelper/src/test/java/za/co/hireahelper/factory/ServiceTypeTest.java
@@ -1,4 +1,28 @@
 package za.co.hireahelper.factory;
 
+import org.junit.jupiter.api.Test;
+import za.co.hireahelper.domain.ServiceType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ServiceTypeTest {
+    @Test
+    void testCreateValidServiceType() {
+        ServiceType serviceType = ServiceTypeFactory.createServiceType("type01", "Plumbing");
+        assertNotNull(serviceType);
+        assertEquals("type01", serviceType.getTypeId());
+        assertEquals("Plumbing", serviceType.getTypeName());
+    }
+
+    @Test
+    void testCreateServiceTypeWithNullId() {
+        ServiceType serviceType = ServiceTypeFactory.createServiceType(null, "Electrician");
+        assertNull(serviceType);
+    }
+
+    @Test
+    void testCreateServiceTypeWithEmptyTypeName() {
+        ServiceType serviceType = ServiceTypeFactory.createServiceType("type02", "");
+        assertNull(serviceType);
+    }
 }
