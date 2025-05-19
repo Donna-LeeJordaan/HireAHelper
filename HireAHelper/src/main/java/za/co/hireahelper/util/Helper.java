d41fa5c3c62f5a31b4da3405fa918e2476d54740
DonnaJ45 <jordaandonnalee@gmail.com>
2025-05-19 18:59:32 +0200

    Factory and Domain class

--- HireAHelper/src/main/java/za/co/hireahelper/util/Helper.java
+++ HireAHelper/src/main/java/za/co/hireahelper/util/Helper.java
@@ -14,6 +14,10 @@ public class Helper {
     public static boolean isValidMobileNumber(String mobileNumber) {
         return mobileNumber != null && mobileNumber.matches("^\\d{10,15}$"); // Basic check for digits, 10-15 length
     }
+
+    public static boolean isValidRate(int rating) {
+        return rating >= 1 && rating <= 5;
+    }
 }
 
 
