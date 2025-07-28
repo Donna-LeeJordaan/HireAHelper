8d08cb425e2a0837340e2763c3632a16677da3fe
Ameeruddin <ameerarai7@gmail.com>
2025-07-28 17:55:29 +0200

    updated code

--- HireAHelper/src/main/java/za/co/hireahelper/domain/Area.java
+++ HireAHelper/src/main/java/za/co/hireahelper/domain/Area.java
@@ -1,34 +1,25 @@
-//Ameeruddin Arai student no: 230190839
-
 package za.co.hireahelper.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.List;
-import java.util.Objects;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "areas")
 public class Area {
 
     @Id
     private String areaId;
+
     private String name;
 
-    // Optional: Link to service providers in this area
-    @OneToMany(mappedBy = "area")
-    private List<ServiceProvider> serviceProviders;
+    @OneToOne(mappedBy = "area", fetch = FetchType.LAZY)
+    private User user;
 
     protected Area() {
-        // Default constructor for JPA
     }
 
     private Area(Builder builder) {
         this.areaId = builder.areaId;
         this.name = builder.name;
-        this.serviceProviders = builder.serviceProviders;
+        this.user = builder.user;
     }
 
     public String getAreaId() {
@@ -39,8 +30,8 @@ public class Area {
         return name;
     }
 
-    public List<ServiceProvider> getServiceProviders() {
-        return serviceProviders;
+    public User getUser() {
+        return user;
     }
 
     @Override
@@ -48,14 +39,14 @@ public class Area {
         return "Area{" +
                 "areaId='" + areaId + '\'' +
                 ", name='" + name + '\'' +
-                ", serviceProviders=" + serviceProviders +
+                ", user=" + (user != null ? user.getUserId() : "null") +
                 '}';
     }
 
     public static class Builder {
         private String areaId;
         private String name;
-        private List<ServiceProvider> serviceProviders;
+        private User user;
 
         public Builder setAreaId(String areaId) {
             this.areaId = areaId;
@@ -67,33 +58,20 @@ public class Area {
             return this;
         }
 
-        public Builder setServiceProviders(List<ServiceProvider> serviceProviders) {
-            this.serviceProviders = serviceProviders;
+        public Builder setUser(User user) {
+            this.user = user;
             return this;
         }
 
         public Builder copy(Area area) {
             this.areaId = area.areaId;
             this.name = area.name;
-            this.serviceProviders = area.serviceProviders;
+            this.user = area.user;
             return this;
         }
 
         public Area build() {
             return new Area(this);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Area)) return false;
-        Area area = (Area) o;
-        return Objects.equals(areaId, area.areaId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(areaId);
     }
 }
