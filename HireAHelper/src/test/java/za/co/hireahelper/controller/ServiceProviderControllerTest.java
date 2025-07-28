453a65681fcde4756c1c4f1f61ea7fc85f55ee72
moega <moegamattauriqosman@gmail.com>
2025-07-28 23:06:17 +0200

    updated

--- HireAHelper/src/test/java/za/co/hireahelper/controller/ServiceProviderControllerTest.java
+++ HireAHelper/src/test/java/za/co/hireahelper/controller/ServiceProviderControllerTest.java
@@ -10,50 +10,52 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import za.co.hireahelper.domain.*;
+import za.co.hireahelper.factory.AreaFactory;
 import za.co.hireahelper.factory.ServiceProviderFactory;
 import za.co.hireahelper.factory.ServiceTypeFactory;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class ServiceProviderControllerTest {
 
-    private static ServiceProvider serviceProvider;
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private static final String BASE_URL = "http://localhost:8080/HireAHelper/serviceProvider";
+    private static ServiceProvider serviceProvider;
+
+    private String getBaseUrl() {
+        return "http://localhost:" + port + "/serviceProvider";
+    }
 
     @BeforeAll
     public static void setUp(){
 
         ServiceType gardener = ServiceTypeFactory.createServiceType("S1", "Gardener");
-        Area area = new Area.Builder()
-                .setAreaId("A1")
-                .setName("Cape Town")
-                .build();
         assertNotNull(gardener, "ServiceType null");
+
+        Area A1 = AreaFactory.createArea("A1","Cape Town");
 
         List<Booking> bookings = new ArrayList<>();
         List<Message> messages = new ArrayList<>();
 
         serviceProvider = ServiceProviderFactory.createServiceProvider("sp1","Saliegh Haroun", "saliegh@gmail.com", "salieghH1234",
-                "0665485568",area, "saliegh.jpeg", "Experienced gardener with 15 years experience.", 668.0,
+                "0665485568",A1, "saliegh.jpeg", "Experienced gardener with 15 years experience.", 668.0,
                 gardener, bookings,messages);
     }
 
     @Test
     void a_create() {
-        String url = BASE_URL + "/create";
+        String url = getBaseUrl() + "/create";
         ResponseEntity <ServiceProvider> postResponse = restTemplate.postForEntity(url, serviceProvider, ServiceProvider.class);
         assertNotNull(postResponse);
         assertEquals(HttpStatus.OK, postResponse.getStatusCode());
@@ -61,20 +63,18 @@ public class ServiceProviderControllerTest {
         ServiceProvider serviceProviderSaved = postResponse.getBody();
         assertNotNull(serviceProviderSaved);
         assertEquals(serviceProvider.getUserId(), serviceProviderSaved.getUserId());
-
         System.out.println("Created: " + serviceProviderSaved);
     }
 
     @Test
     void b_read() {
-        String url = BASE_URL + "/read/" + serviceProvider.getUserId();
+        String url = getBaseUrl() + "/read/" + serviceProvider.getUserId();
         ResponseEntity<ServiceProvider> response = restTemplate.getForEntity(url, ServiceProvider.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
 
+        assertEquals(HttpStatus.OK, response.getStatusCode());
         ServiceProvider serviceProviderRead = response.getBody();
         assertNotNull(serviceProviderRead);
         assertEquals(serviceProvider.getUserId(), serviceProviderRead.getUserId());
-
         System.out.println("Read: " + serviceProviderRead);
     }
 
@@ -85,26 +85,25 @@ public class ServiceProviderControllerTest {
                 .setName("Moegamat Saliegh Haroun")
                 .build();
 
-        String url = BASE_URL + "/update";
+        String url = getBaseUrl() + "/update";
         restTemplate.put(url, updatedServiceProvider);
 
-        ResponseEntity<ServiceProvider> response = restTemplate.getForEntity(BASE_URL + "/read/" + updatedServiceProvider.getUserId(), ServiceProvider.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        ResponseEntity<ServiceProvider> response = restTemplate.getForEntity(getBaseUrl() + "/read/" + updatedServiceProvider.getUserId(), ServiceProvider.class);
 
+        assertEquals(HttpStatus.OK, response.getStatusCode());
         ServiceProvider serviceProviderUpdated = response.getBody();
         assertNotNull(serviceProviderUpdated);
         assertEquals("Moegamat Saliegh Haroun", serviceProviderUpdated.getName());
-
         System.out.println("Updated:" + serviceProviderUpdated);
     }
 
     @Disabled
     @Test
     void e_delete() {
-        String url = BASE_URL + "/delete/" + serviceProvider.getUserId();
+        String url = getBaseUrl() + "/delete/" + serviceProvider.getUserId();
         restTemplate.delete(url);
 
-        ResponseEntity<ServiceProvider> response = restTemplate.getForEntity(BASE_URL + "/read/" + serviceProvider.getUserId(), ServiceProvider.class);
+        ResponseEntity<ServiceProvider> response = restTemplate.getForEntity(getBaseUrl() + "/read/" + serviceProvider.getUserId(), ServiceProvider.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
         assertNull(response.getBody());
@@ -113,12 +112,13 @@ public class ServiceProviderControllerTest {
 
     @Test
     void d_getAll() {
-        String url = BASE_URL + "/all";
+        String url = getBaseUrl() + "/all";
         ResponseEntity<ServiceProvider[]> response = restTemplate.getForEntity(url, ServiceProvider[].class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
         ServiceProvider[] serviceProviders = response.getBody();
         assertNotNull(serviceProviders);
+        assertTrue(serviceProviders.length > 0);
 
         System.out.println("All ServiceProviders:");
         for (ServiceProvider s : serviceProviders) {
--- HireAHelper/src/test/java/za/co/hireahelper/service/ServiceProviderServiceTest.java
+++ HireAHelper/src/test/java/za/co/hireahelper/service/ServiceProviderServiceTest.java
@@ -14,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import za.co.hireahelper.domain.*;
 import za.co.hireahelper.factory.ServiceProviderFactory;
 import za.co.hireahelper.factory.ServiceTypeFactory;
+import za.co.hireahelper.repository.AreaRepository;
+import za.co.hireahelper.repository.ServiceTypeRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +29,13 @@ public class ServiceProviderServiceTest {
     @Autowired
     private ServiceProviderService service;
 
-    ServiceType gardener = ServiceTypeFactory.createServiceType("S1", "Gardener");
+    @Autowired
+    private AreaRepository areaRepository;
+
+    ServiceType serviceType = new ServiceType.Builder()
+            .setTypeId("S1")
+            .setTypeName("Gardener")
+            .build();
     Area area = new Area.Builder()
             .setAreaId("A1")
             .setName("Cape Town")
@@ -37,11 +46,13 @@ public class ServiceProviderServiceTest {
 
     private ServiceProvider serviceProvider = ServiceProviderFactory.createServiceProvider(
             "sp1","Saliegh Haroun","saliegh@gmail.com","SalieghH1234","0665485568",
-            area,"saliegh.jpeg", "Experienced gardener with 15 years experience",668.0,gardener,bookings,
+            area,"saliegh.jpeg", "Experienced gardener with 15 years experience",668.0,serviceType ,bookings,
             messages);
 
     @Test
     void a_create() {
+        areaRepository.save(serviceProvider.getArea());
+
         ServiceProvider created = service.create(serviceProvider);
         assertNotNull(created);
         System.out.println("Created:" + created);
@@ -63,21 +74,22 @@ public class ServiceProviderServiceTest {
 
         ServiceProvider updated = service.update(updatedServiceProvider);
         assertNotNull(updated);
+        assertEquals("Moegamat Saliegh Haroun", updatedServiceProvider.getName());
         System.out.println("Updated:" + updated);
     }
 
     @Test
     void e_getAll() {
-        var allServiceProviders= service.getAll();
-        assertNotNull(allServiceProviders);
-        System.out.println("All Service Providers:" + allServiceProviders);
+        List<ServiceProvider> serviceProviders= service.getAll();
+        assertNotNull(serviceProviders);
+        System.out.println("All Service Providers:" + serviceProviders);
     }
 
     @Test
     void f_delete() {
         boolean deleted = service.delete(serviceProvider.getUserId());
         assertTrue(deleted);
-        System.out.println("Deleted:" + deleted);
+        System.out.println("Deleted Service Provider with userId:" + serviceProvider.getUserId());
     }
 }
 
