3d700c5faf14b930a2ecf0c3d66fe03389fefdc2
gabrielkiewietz <kiewietzg16@gmail.com>
2025-08-01 10:32:08 +0200

    new

--- HireAHelper/src/test/java/za/co/hireahelper/controller/MessageControllerTest.java
+++ HireAHelper/src/test/java/za/co/hireahelper/controller/MessageControllerTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MessageControllerTest {
 
     private static Message message;
-
+//3
     @Autowired
     private TestRestTemplate restTemplate;
 
