f0e2d11811034113d6cdd2c2ecc9ccae59d7bebd
Saadiqah Hendricks <saadiqahhendricks2909@gmail.com>
2025-08-22 11:56:52 +0200

    update

--- HireAHelper/src/frontend/src/main.jsx
+++ HireAHelper/src/frontend/src/main.jsx
@@ -2,22 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
 import ServiceProviderDashboard from './serviceProvider/ServiceProviderDashboard.jsx'
-import ClientDashboard from './client/ClientDashboard.jsx'
 import ServiceProviderRegister from "./serviceProvider/ServiceProviderRegister.jsx";
 import ClientRegister from "./client/ClientRegister.jsx";
+import Home from "./Home.jsx";
+import AuthPage from "./AuthPage.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
 
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/auth" element={<AuthPage />} />
                 <Route path="/serviceProviderRegister" element={<ServiceProviderRegister />} />
                 <Route path="/service-provider-dashboard" element={<ServiceProviderDashboard />} />
                 <Route path="/clientRegister" element={<ClientRegister />} />
-                <Route path="/client-dashboard" element={<ClientDashboard />} />
 
             </Routes>
         </BrowserRouter>
