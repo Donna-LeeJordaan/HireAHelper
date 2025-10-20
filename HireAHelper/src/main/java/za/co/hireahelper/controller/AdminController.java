2c0cf8a9f8d15cc2b71f084a6a0ab4c54460a36b
Saadiqah Hendricks <saadiqahhendricks2909@gmail.com>
2025-10-20 16:14:46 +0200

    updated code.

--- HireAHelper/src/main/java/za/co/hireahelper/controller/AdminController.java
+++ HireAHelper/src/main/java/za/co/hireahelper/controller/AdminController.java
@@ -3,8 +3,12 @@
 package za.co.hireahelper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import za.co.hireahelper.domain.Admin;
+import za.co.hireahelper.domain.UserPrincipal;
 import za.co.hireahelper.service.AdminService;
 import java.util.List;
 
@@ -43,5 +47,17 @@ public class AdminController {
     @GetMapping("/all")
     public List<Admin> getAll() {
         return adminService.getAll();
+    }
+
+    @GetMapping("/current-admin")
+    public ResponseEntity<?> getCurrentAdmin(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        String userId = principal.getUser().getUserId();
+        Admin admin = adminService.read(userId);
+        return ResponseEntity.ok(admin);
     }
 }
